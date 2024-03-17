8c377bda1055854c3613a3b52ab007c1705dc82c
Kevin4345 <125806726+Kevin4345@users.noreply.github.com>
2024-03-17 07:15:18 -0700

    learning Javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("hello world")
+let programminglanguages = ["hmtl","python","css","java","javascript"]
+programminglanguages[0]
+/*
+This is my first comment
+*/
+// This is my line comment
+let firstname  =  "Kevin "
+let secondname = "Kevo"
+let fullname = firstname +" " + secondname
+let kev = "3"
+let vek = 3
+kev == vek //returns true
+kev === vek//returns false
+// !   !==
+!(kev == vek)
+kev!== vek
+
